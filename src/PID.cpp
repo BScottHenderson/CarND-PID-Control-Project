3764b1d7df34fcd265b5377c345d68062a840e09
Scott Henderson <henders999@gmail.com>
2018-12-06 15:17:55 -0800

    Changed the implementation to use the existing class structure now that I actually understand it.

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,35 +10,25 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
-  this->p_error = 0.0;
-  this->i_error = 0.0;
-  this->d_error = 0.0;
-  this->Kp = Kp;
-  this->Ki = Ki;
-  this->Kd = Kd;
-  this->prev_cte = std::numeric_limits<double>::min();
+bool  first_error = true;
+
+void PID::Init(double _Kp, double _Ki, double _Kd) {
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+  Kp = _Kp;
+  Ki = _Ki;
+  Kd = _Kd;
 }
 
-void PID::UpdateError(double cte) {
-  this->p_error += cte * this->Kp;
-  this->i_error += cte * this->Ki;
-  this->d_error += cte * this->Kd;
+void PID::UpdateError(double _cte) {
+  d_error = (first_error) ? 0.0 : _cte - p_error;
+  p_error = _cte;
+  i_error += _cte;
+  first_error = false;
 }
 
 double PID::TotalError() {
-  return this->p_error + this->i_error + this->d_error;
-}
-
-double PID::Value(double cte) {
-  // Calculate delta CTE.
-  double  diff_cte =
-    (std::numeric_limits<double>::min() == prev_cte) ? 0.0 : cte - prev_cte;
-  prev_cte = cte;
-
-  // Add current CTE to the total.
-  UpdateError(cte);
-
   // PID formula
-  return -Kp * cte - Kd * diff_cte - Ki * TotalError();
+  return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
